a3e54ce5aea0f538b4ca1561e318f798294952e3
Rafael Coelho <rafallcoelho@gmail.com>
2018-12-18 19:26:28 -0300

    Separa pontos por dia e os agrupa por minuto

--- encontros.R
+++ encontros.R
@@ -1,25 +1,35 @@
+library(dplyr)
+
 print("Lendo csv dos pontos.")
 points = read.csv("~/Documents/mate05/points.csv")
-print("csv lido")
 
 points.by.day = list()
-
 date = ""
 
 for (i in 1:30) {
-	print(paste("Dia", i))
 	if(i < 10) {
 		date = paste("2009-06-0", as.character(i), sep = "")
 	}
 	else {
 		date = paste("2009-06-", as.character(i), sep = "")
 	}
-	print(date)
-	print(subset(points, data.days == date))
-	points.by.day[[date]] = subset(points, date.days == date)
+	points.by.day[[date]] = subset(points, date.str == date)
 }
 
-print(points.by.day$'2009-06-04')
+#points.by.day.min = list()
+
+for (i in 1:30) {
+	points.by.day[[i]]$time = as.character(sapply(as.character(points.by.day[[i]]$time), substr, 1, 5))
+}
+
+for (i in 1:30) {
+	points.by.day[[i]] = points.by.day[[i]] %>%
+		group_by(user, time) %>%
+		summarize(lon = round(mean(lon), 5), lat = round(mean(lat), 5)) %>%
+		ungroup()
+}
 
 
+
+head(points.by.day[[1]]$lat)
 	
