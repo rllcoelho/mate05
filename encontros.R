b1affe852da76c545dae5d940e347d333653d226
Rafael Coelho <rafallcoelho@gmail.com>
2018-12-18 20:34:33 -0300

    Calcula distânia entre pontos

--- encontros.R
+++ encontros.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(geosphere)
 
 print("Lendo csv dos pontos.")
 points = read.csv("~/Documents/mate05/points.csv")
@@ -25,11 +26,15 @@ for (i in 1:30) {
 for (i in 1:30) {
 	points.by.day[[i]] = points.by.day[[i]] %>%
 		group_by(user, time) %>%
-		summarize(lon = round(mean(lon), 5), lat = round(mean(lat), 5)) %>%
+		summarize(lon = round(mean(lon), 6), lat = round(mean(lat), 6)) %>%
 		ungroup()
+
+	for (x in 1:nrow(points.by.day[[i]])-1) {
+		for (y in x+1:nrow(points.by.day[[i]])) {
+			print(distm(points.by.day[[i]][x,c(3:4)], distm(points.by.day[[i]][y,c(3:4)], fun = distHaversine)))
+		}
+	}
 }
-
-
 
 head(points.by.day[[1]]$lat)
 	
