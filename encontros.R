dbfd44a68fa7c336c3ed0b16cea6ff70b55769b9
Rafael Coelho <rafallcoelho@gmail.com>
2018-12-19 17:38:37 -0300

    Termina script do jeito burro

--- encontros.R
+++ encontros.R
@@ -23,18 +23,50 @@ for (i in 1:30) {
 	points.by.day[[i]]$time = as.character(sapply(as.character(points.by.day[[i]]$time), substr, 1, 5))
 }
 
+encontros = list()
+
+achaEncontros = function(ponto) {
+
+}
+
 for (i in 1:30) {
 	points.by.day[[i]] = points.by.day[[i]] %>%
 		group_by(user, time) %>%
 		summarize(lon = round(mean(lon), 6), lat = round(mean(lat), 6)) %>%
 		ungroup()
 
-	for (x in 1:nrow(points.by.day[[i]])-1) {
-		for (y in x+1:nrow(points.by.day[[i]])) {
-			print(distm(points.by.day[[i]][x,c(3:4)], distm(points.by.day[[i]][y,c(3:4)], fun = distHaversine)))
+	dia.atual = points.by.day[[i]]
+
+	print(i)
+	print(nrow(dia.atual))
+	Sys.sleep(1000)
+	
+	
+
+	for (x in 1:(nrow(dia.atual)-1)) {
+		user1 = dia.atual[x,'user']
+		for (y in (x+1):nrow(dia.atual)) {
+			user2 = dia.atual[y, 'user']
+			print(user1)
+			print(user2)
+			if (user2 != user1) {
+				ponto1 = data.frame(dia.atual[x,c(3:4)])
+				ponto2 = data.frame(dia.atual[y,c(3:4)])
+				dist = distm(c(ponto1[1,1], ponto1[1,2]), c(ponto2[1,1], ponto2[1,2]) , fun = distHaversine)
+				if(dist > 50) {
+					nome.encontro = paste(user1, ',', user2, sep='')
+					if (is.null(encontros[[nome.encontro]])) {
+						encontros[[nome.encontro]] = 1
+					}
+					else {
+						encontros[[nome.encontro]] = encontros[[nome.encontro]] + 1
+					}
+				}
+			}
 		}
 	}
+	write.csv(encontros, paste('~/Documents/mate05/encontrosAteDia', i, '.csv', sep='')
 }
 
-head(points.by.day[[1]]$lat)
+write.csv(encontros, '~/Documents/mate05/encontros.csv')
 	
